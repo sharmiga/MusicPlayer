5404dff0d8b002c8b7b314a1ad873c2426465060
sharmiga <717821s152@kce.ac.in>
2024-04-18 13:47:16 +0530

    intial commit

---
+++ src/components/Img.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Img() {
+  return (
+   <>
+      <div className="img">
+      <div className='quotes'>
+        <h2 className='rh'>Musical Mirage</h2>
+        <h3 className='qte'>Music is the divine way to tell beautiful, poetic things to the heart.</h3>
+       <h4 className='Quote'> </h4>
+     </div>
+      </div>
+       
+     </>
+   
+  );
+}
+export default Img;
